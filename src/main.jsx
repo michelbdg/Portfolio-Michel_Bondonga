301f41ed8bfbf6a6db5c551f02779c1333a00525
michelbdg <114735296+michelbdg@users.noreply.github.com>
2024-02-09 12:18:15 +0100

    ajout du React dom router

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,32 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import Home from './pages/Home';
 import './index.css'
 
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/competences",
+    element: <Home />,
+  },
+  {
+    path: "/contact",
+    element: <Home />,
+  },
+  {
+    path: "/portfolio",
+    element: <Home />,
+  },
+]);
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
