f4d4357d3496e4cfa2133df92e34bb223f0baf57
michelbdg <114735296+michelbdg@users.noreply.github.com>
2024-02-09 12:17:13 +0100

    ajout du composants du navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,24 +1,27 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
+import MMB from "../assets/images/MMB.png"
 export default function Navbar(){
 
 
     return(
-        <header>
-            <nav class="navbar navbar-expand-lg navbar-light fixed-top py-3" id="mainNav">
-                <div class="container px-4 px-lg-5">
-                    <a class="navbar-brand" href="#page-top">Start Bootstrap</a>
-                    <button class="navbar-toggler navbar-toggler-right" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
-                    <div class="collapse navbar-collapse" id="navbarResponsive">
-                        <ul class="navbar-nav ms-auto my-2 my-lg-0">
-                            <li class="nav-item"><a class="nav-link" href="#about">About</a></li>
-                            <li class="nav-item"><a class="nav-link" href="#services">Services</a></li>
-                            <li class="nav-item"><a class="nav-link" href="#portfolio">Portfolio</a></li>
-                            <li class="nav-item"><a class="nav-link" href="#contact">Contact</a></li>
+        <>
+            <nav className="navbar navbar-expand-lg navbar-light fixed-top py-3" id="mainNav">
+                <div className="container px-4 px-lg-5">
+                    <Link className="navbar-brand" to="/">
+                        <img  className="card-img-top w-50" src={ MMB } alt="Logo" />
+                    </Link>
+                    <button className="navbar-toggler navbar-toggler-right" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span className="navbar-toggler-icon"></span></button>
+                    <div className="collapse navbar-collapse" id="navbarResponsive">
+                        <ul className="navbar-nav ms-auto my-2 my-lg-0">
+                            <li className="nav-item"><a className="nav-link" href="#about">About</a></li>
+                            <li className="nav-item"><a className="nav-link" href="#services">Services</a></li>
+                            <li className="nav-item"><a className="nav-link" href="#portfolio">Portfolio</a></li>
+                            <li className="nav-item"><a className="nav-link" href="#contact">Contact</a></li>
                         </ul>
                     </div>
                 </div>
             </nav>
-        </header>
+        </>
     )
 }
