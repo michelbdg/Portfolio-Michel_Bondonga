b91f9062b83ad3319b5bd65058a5a96f2ffcf7ab
michelbdg <114735296+michelbdg@users.noreply.github.com>
2024-02-11 01:05:45 +0100

    Ajout des composant au template Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,22 @@
 import React from "react";
 import Navbar from "../components/Navbar";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import About from "../components/about";
+import Service from "../components/Service";
+import Portfolio from "./Portfolio";
+import File from "../components/File";
 export default function Home(){
 
     return(
         <>
             <Navbar/>
+            <Header/>
+            <About/>
+            <Service/>
+            <Portfolio/>
+            <File/>
+            <Footer/>
         </>
     )
 }
